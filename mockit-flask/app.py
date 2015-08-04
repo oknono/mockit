ed4f9439c7e50b4cb4e47f83bc89a7abd8ed99b9
Kirsten Korevaar <kirsten@eml.cc>
2015-08-04 10:26:23 -0400

    working flask app

--- mockit-flask/app.py
+++ mockit-flask/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, redirect, request
+from mockit import *
 
 app = Flask(__name__)
 app.debug = True
@@ -6,6 +7,16 @@ app.debug = True
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
+
+@app.route('/randompost', methods=['POST'])
+def randompost():
+    subreddit = request.form['subreddit']
+    title = generate_post_title(subreddit)
+
+    if not subreddit:
+        return redirect(url_for('index'))
+
+    return render_template('randompost.html', title=title, post="Work in Progess", subreddit=subreddit)
 
 if __name__ == '__main__':
     app.run()
---
+++ mockit-flask/markov.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from random import choice
+import sys
+
+def generateModel(text, order):
+    model = {}
+    for i in range(0, len(text) - order):
+        fragment = text[i:i+order]
+        next_letter = text[i+order]
+        if fragment not in model:
+            model[fragment] = {}
+        if next_letter not in model[fragment]:
+            model[fragment][next_letter] = 1
+        else:
+            model[fragment][next_letter] += 1
+    return model
+
+def getNextCharacter(model, fragment):
+    letters = []
+    for letter in model[fragment].keys():
+        for times in range(0, model[fragment][letter]):
+            letters.append(letter)
+    return choice(letters)
+
+def generateText(text, order, length):
+    model = generateModel(text, order)
+    currentFragment = text[0:order]
+    output = ""
+    for i in range(0, length-order):
+        newCharacter = getNextCharacter(model, currentFragment)
+        output += newCharacter
+        currentFragment = currentFragment[1:] + newCharacter
+    return output
+
+if __name__ == "__main__":
+    text = "It doesn't hurt me Do you wanna feel how it feels?  Do you wanna know, know that it doesn't hurt me? Do you wanna hear about the deal that I'm making?  You It's you and me And if I only could I'd make a deal with God And I'd get him to swap our places Be running up that road Be running up that hill Be running up that building See if I only could, oh You don't wanna hurt me But see how deep the bullet lies Unaware I'm tearing you asunder Oh, there is thunder in our hearts Is there so much hate for the ones we love? Well tell me, we both matter, don't we?  You It's you and me It's you and me won't be unhappy And if I only could I'd make a deal with God And I'd get him to swap our places Be running up that road Be running up that hill Be running up that building Say, if I only could, oh You It's you and me It's you and me won't be unhappy Oh c'mon, baby, c'mon darling Let me steal this moment from you now. C'mon, angel, c'mon, c'mon, darling Let's exchange the experience, oh And if I only could I'd make a deal with God And I'd get him to swap our places I'd be running up that road Be running up that hill With no problems See and if I only could I'd make a deal with God And I'd get him to swap our places Be running up that road Be running up that hill With no problems So if I only could I'd make a deal with God And I'd get him to swap our places I'd be running up that road Be running up that hillWith no problems So if I only could  Be running up that hill  With no problems If I only could, I'd be running up that hill If I only could, I'd be running up that hill"
+
+    generateText(text, int(sys.argv[1]), int(sys.argv[2]))
